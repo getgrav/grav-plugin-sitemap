9fe10902270953038526976648c3f30e12edaf5e
Giansimon <giansimon.diblas@gmail.com>
2015-08-02 09:33:06 +0200

    avoid display in sitemap unpublished pages

--- sitemap.php
+++ sitemap.php
@@ -67,7 +67,7 @@ class SitemapPlugin extends Plugin
         foreach ($routes as $route => $path) {
             $page = $pages->get($path);
 
-            if ($page->routable() && !in_array($page->route(), $ignores)) {
+            if ($page->published() && $page->routable() && !in_array($page->route(), $ignores)) {
                 $entry = new SitemapEntry();
                 $entry->location = $page->permaLink();
                 $entry->lastmod = date('Y-m-d', $page->modified());

31a84f9b75cd5fd3ce0013287719cca9548b5761
Andy Miller <rhuk@mac.com>
2020-07-01 11:11:15 -0600

    Allow setting by constructor
    Signed-off-by: Andy Miller <rhuk@mac.com>

--- classes/sitemapentry.php
+++ classes/sitemapentry.php
@@ -8,4 +8,22 @@ class SitemapEntry
     public $changefreq;
     public $priority;
     public $image;
+
+    /**
+     * SitemapEntry constructor.
+     *
+     * @param null $location
+     * @param null $lastmod
+     * @param null $changefreq
+     * @param null $priority
+     * @param null $image
+     */
+    public function __construct($location = null, $lastmod = null, $changefreq = null, $priority = null, $image = null)
+    {
+        $this->location = $location;
+        $this->lastmod = $lastmod;
+        $this->changefreq = $changefreq;
+        $this->priority = $priority;
+        $this->image = $image;
+    }
 }
